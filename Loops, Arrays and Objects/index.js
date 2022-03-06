7a5c1be47bd15c41cfdea1d5892239934be8ff14
koulinn <drdverzola@gmail.com>
2022-03-06 15:41:35 +0000

    Exercise booklist and checkEvenOdd

---
+++ Loops, Arrays and Objects/index.js
@@ -0,0 +1,46 @@
+const checkEvenOdd = () => {
+  for (let i = 1; i < 21; i++) {
+    if (i % 2 === 0) {
+      console.log(`${i} is even`);
+    } else {
+      console.log(`${i} is odd`);
+    }
+  }
+};
+
+checkEvenOdd();
+
+const bookList = [
+  {
+    title: "title1",
+    author: "author1",
+    alreadyRead: true,
+  },
+  {
+    title: "title2",
+    author: "author2",
+    alreadyRead: true,
+  },
+  {
+    title: "title3",
+    author: "author3",
+    alreadyRead: false,
+  },
+  {
+    title: "title4",
+    author: "author4",
+    alreadyRead: true,
+  },
+];
+
+const checkReadBooks = (list) => {
+  list.forEach((book) => {
+    if (book.alreadyRead) {
+      console.log(`Book ${book.title} from ${book.author} already read.`);
+    } else {
+      console.log(`Book ${book.title} from ${book.author} not read.`);
+    }
+  });
+};
+
+// checkReadBooks(bookList);
